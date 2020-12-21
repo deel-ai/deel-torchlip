dc4a03fa6cae4c1214e77d27368700ed9a4c1013
Mikaël Capelle <capelle.mikael@gmail.com>
2020-12-21 10:17:06 +0100

    Move sqrt_with_eps to its own file.

--- deel/torchlip/utils/__init__.py
+++ deel/torchlip/utils/__init__.py
@@ -6,48 +6,17 @@
 Contains utility functions.
 """
 
-from typing import Any, Optional, Tuple
+from typing import Optional, Tuple
 
 import numpy as np
 import torch
 import torch.autograd
 
+from .sqrt_eps import sqrt_with_gradeps  # noqa: F401
+
 DEFAULT_NITER_BJORCK = 15
 DEFAULT_NITER_SPECTRAL = 3
 DEFAULT_NITER_SPECTRAL_INIT = 10
-
-
-class SqrtEpsGrad(torch.autograd.Function):
-    """
-    Small class to avoid division by zero when computing the gradient
-    of the sqrt function.
-    """
-
-    @staticmethod
-    def forward(ctx: Any, input: Any, eps: float) -> torch.Tensor:  # type: ignore
-        ctx.save_for_backward(input)
-        ctx.eps = eps
-        return torch.sqrt(input)
-
-    @staticmethod
-    def backward(ctx: Any, grad_output):  # type: ignore
-        (input,) = ctx.saved_tensors
-        return grad_output / (2 * (input + ctx.eps)), None
-
-
-def sqrt_with_gradeps(input: torch.Tensor, eps: float = 1e-6) -> torch.Tensor:
-    """
-    Square-root of input with a valid gradient at 0.
-
-    Args:
-        input: Tensor of arbitrary shape.
-        eps: Value to add to the input when computing gradient (must be positive).
-
-    Returns:
-        A tensor whose value is the square-root of the input but whose associated
-        autograd functions is `SqrtEpsGrad`.
-    """
-    return SqrtEpsGrad.apply(input, eps)  # type: ignore
 
 
 def evaluate_lip_const(
