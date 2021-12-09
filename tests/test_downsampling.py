4837a7be08ac9d7bebc948c28ee8f1649bef1b6a
justin.plakoo <justin.plakoo@irt-saintexupery.com>
2021-12-09 15:29:55 +0100

    style: Test precommit

--- tests/test_downsampling.py
+++ tests/test_downsampling.py
@@ -32,7 +32,7 @@ from deel.torchlip.functional import invertible_downsample
 
 def test_invertible_downsample():
 
-    # 1D input case
+    # 1D input
     x = torch.tensor([[[1, 2, 3, 4], [5, 6, 7, 8]]])
     x = invertible_downsample(x, (2,))
     assert x.shape == (1, 4, 2)
@@ -40,13 +40,13 @@ def test_invertible_downsample():
     # TODO: Check this.
     tt.assert_equal(x, torch.tensor([[[1, 2], [3, 4], [5, 6], [7, 8]]]))
 
-    # 2D input case
+    # 2D input
     x = torch.rand(10, 1, 128, 128)
     assert invertible_downsample(x, (4, 4)).shape == (10, 16, 32, 32)
 
     x = torch.rand(10, 4, 64, 64)
     assert invertible_downsample(x, (2, 2)).shape == (10, 16, 32, 32)
 
-    # 3D input case
+    # 3D input
     x = torch.rand(10, 2, 128, 64, 64)
     assert invertible_downsample(x, 2).shape == (10, 16, 64, 32, 32)
