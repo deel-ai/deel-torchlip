b5f1c7282a299d40b0db5cc0c88d530ac285778d
justin.plakoo <justin.plakoo@irt-saintexupery.com>
2021-12-09 14:41:08 +0100

    Resolving pre-commit-hook changes

--- deel/torchlip/modules/upsampling.py
+++ deel/torchlip/modules/upsampling.py
@@ -20,7 +20,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
 # Copyright IRT Antoine de Saint Exupéry et Université Paul Sabatier Toulouse III - All
 # rights reserved. DEEL is a research program operated by IVADO, IRT Saint Exupéry,
 # CRIAQ and ANITI - https://www.deel.ai/
