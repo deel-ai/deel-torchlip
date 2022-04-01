cd622295ba9a7d6ba2f1e3a22061bbd0f609bc7e
cofri <cofri@users.noreply.github.com>
2022-04-01 16:27:48 +0200

    fix: new correction factor for Conv layers
    The Lipschitz constant of conv layers must be corrected with a specific
    factor. This factor is updated.

--- deel/torchlip/utils/lconv_norm.py
+++ deel/torchlip/utils/lconv_norm.py
@@ -43,10 +43,9 @@ def compute_lconv_coef(
     k1, k2 = kernel_size
     h, w = input_shape[-2:]
 
-    k1_div2 = (k1 - 1) / 2
-    k2_div2 = (k2 - 1) / 2
-
     if stride == 1:
+        k1_div2 = (k1 - 1) / 2
+        k2_div2 = (k2 - 1) / 2
         coefLip = np.sqrt(
             (w * h)
             / ((k1 * h - k1_div2 * (k1_div2 + 1)) * (k2 * w - k2_div2 * (k2_div2 + 1)))
@@ -54,21 +53,7 @@ def compute_lconv_coef(
     else:
         sn1 = strides[0]
         sn2 = strides[1]
-        ho = np.floor(h / sn1)
-        wo = np.floor(w / sn2)
-        alphabar1 = np.floor(k1_div2 / sn1)
-        alphabar2 = np.floor(k2_div2 / sn2)
-        betabar1 = k1_div2 - alphabar1 * sn1
-        betabar2 = k2_div2 - alphabar2 * sn2
-        zl1 = (alphabar1 * sn1 + 2 * betabar1) * (alphabar1 + 1) / 2
-        zl2 = (alphabar2 * sn2 + 2 * betabar2) * (alphabar2 + 1) / 2
-        gamma1 = h - 1 - sn1 * np.ceil((h - 1 - k1_div2) / sn1)
-        gamma2 = w - 1 - sn2 * np.ceil((w - 1 - k2_div2) / sn2)
-        alphah1 = np.floor(gamma1 / sn1)
-        alphaw2 = np.floor(gamma2 / sn2)
-        zr1 = (alphah1 + 1) * (k1_div2 - gamma1 + sn1 * alphah1 / 2.0)
-        zr2 = (alphaw2 + 1) * (k2_div2 - gamma2 + sn2 * alphaw2 / 2.0)
-        coefLip = np.sqrt((h * w) / ((k1 * ho - zl1 - zr1) * (k2 * wo - zl2 - zr2)))
+        coefLip = np.sqrt(1.0 / (np.ceil(k1 / sn1) * np.ceil(k2 / sn2)))
 
     return coefLip  # type: ignore
 
