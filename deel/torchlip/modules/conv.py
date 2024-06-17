184bdd4643340971f4559928e9a1a8980c712d3c
Mathieu SERRURIER <serrurie@tachikoma.irit.fr>
2024-06-17 11:28:39 +0200

    spectral conv 1d

--- deel/torchlip/modules/conv.py
+++ deel/torchlip/modules/conv.py
@@ -37,6 +37,101 @@ from ..utils import lconv_norm
 from .module import LipschitzModule
 
 
+class SpectralConv1d(torch.nn.Conv1d, LipschitzModule):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: _size_2_t,
+        stride: _size_2_t = 1,
+        padding: _size_2_t = 0,
+        dilation: _size_2_t = 1,
+        groups: int = 1,
+        bias: bool = True,
+        padding_mode: str = "zeros",
+        k_coef_lip: float = 1.0,
+        niter_spectral: int = DEFAULT_NITER_SPECTRAL,
+        niter_bjorck: int = DEFAULT_NITER_BJORCK,
+    ):
+        """
+        This class is a Conv2d Layer constrained such that all singular of it's kernel
+        are 1. The computation based on BjorckNormalizer algorithm. As this is not
+        enough to ensure 1-Lipschitz a coercive coefficient is applied on the
+        output.
+        The computation is done in three steps:
+
+        1. reduce the largest singular value to 1, using iterated power method.
+        2. increase other singular values to 1, using BjorckNormalizer algorithm.
+        3. divide the output by the Lipschitz bound to ensure k-Lipschitz.
+
+        Args:
+            in_channels (int): Number of channels in the input image
+            out_channels (int): Number of channels produced by the convolution
+            kernel_size (int or tuple): Size of the convolving kernel
+            stride (int or tuple, optional): Stride of the convolution.
+            padding (int or tuple, optional): Zero-padding added to both sides of
+                the input.
+            padding_mode (string, optional): ``'zeros'``, ``'reflect'``,
+                ``'replicate'`` or ``'circular'``. Default: ``'zeros'``
+            dilation (int or tuple, optional): Spacing between kernel elements.
+            groups (int, optional): Number of blocked connections from input
+                channels to output channels.
+            bias (bool, optional): If ``True``, adds a learnable bias to the
+                output.
+            k_coef_lip: Lipschitz constant to ensure.
+            niter_spectral: Number of iteration to find the maximum singular value.
+            niter_bjorck: Number of iteration with BjorckNormalizer algorithm.
+
+        This documentation reuse the body of the original torch.nn.Conv2D doc.
+        """
+        # if not ((dilation == (1, 1)) or (dilation == [1, 1]) or (dilation == 1)):
+        #     raise RuntimeError("NormalizedConv does not support dilation rate")
+        # if padding_mode != "same":
+        #     raise RuntimeError("NormalizedConv only support padding='same'")
+
+        torch.nn.Conv1d.__init__(
+            self,
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            bias=bias,
+            padding_mode=padding_mode,
+        )
+        LipschitzModule.__init__(self, k_coef_lip)
+
+        torch.nn.init.orthogonal_(self.weight)
+        if self.bias is not None:
+            self.bias.data.fill_(0.0)
+
+        spectral_norm(
+            self,
+            name="weight",
+            n_power_iterations=niter_spectral,
+        )
+        bjorck_norm(self, name="weight", n_iterations=niter_bjorck)
+        lconv_norm(self)
+        self.register_forward_pre_hook(self._hook)
+
+    def vanilla_export(self):
+        layer = torch.nn.Conv1d(
+            in_channels=self.in_channels,
+            out_channels=self.out_channels,
+            kernel_size=self.kernel_size,
+            stride=self.stride,
+            padding=self.padding,
+            dilation=self.dilation,
+            groups=self.groups,
+            bias=self.bias is not None,
+            padding_mode=self.padding_mode,
+        )
+        layer.weight.data = self.weight.detach()
+        if self.bias is not None:
+            layer.bias.data = self.bias.detach()
+        return layer
+
+
 class SpectralConv2d(torch.nn.Conv2d, LipschitzModule):
     def __init__(
         self,
