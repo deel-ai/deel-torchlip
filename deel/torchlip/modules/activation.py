simplify householder reshape computation + correct test

deel/torchlip/modules/activation.py
@@ -250,9 +250,8 @@ class HouseHolder(nn.Module, LipschitzModule):
                 raise ValueError(f"Unknown initializer {theta_initializer}")
 
     def forward(self, z, axis=1):
-        theta = self.theta.to(z.device).view(1, -1)
-        for _ in range(len(z.shape) - len(theta.shape)):
-            theta = theta.unsqueeze(-1)
+        theta_shape = (1, -1) + (1,) * (len(z.shape) - 2)
+        theta = self.theta.to(z.device).view(theta_shape)
         x, y = z.split(z.shape[axis] // 2, axis)
         selector = (x * torch.sin(0.5 * theta)) - (y * torch.cos(0.5 * theta))
 